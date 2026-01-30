97c267ff9fc1f644f535401988c043e3239d49f4
AlexCodreanu23 <AlexCosmin2377@gmail.com>
2026-01-30 14:53:40 +0200

    feat: separating build and run scripts

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+echo "Building Java projects..."
+cd common-rmi && mvn clean install -q && cd ..
+cd c01-backend-javalin && mvn clean package -q && cd ..
+cd c03-ejb-mdb-rmi-client && mvn clean package -q && cd ..
+cd c04-rmi-server-1 && mvn clean package -q && cd ..
+cd c05-rmi-server-2 && mvn clean package -q && cd ..
+echo "Build complete!"
